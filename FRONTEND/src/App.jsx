473b2c32c24f81bd3407e7223f90c9feb86cfd13
Pankaj kumar <jaat548986@gmail.com>
2025-12-05 12:51:46 +0530

    Fix import case for userDataContext

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -3,7 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import SignUp from "./pages/SignUp.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import Customize from "./pages/Customize.jsx";
-import { userDataContext } from "./context/userContext.jsx";
+import { userDataContext } from "./context/UserContext.jsx";
 import Home from "./pages/Home.jsx";
 import Customize2 from "./pages/Customize2.jsx";
 
